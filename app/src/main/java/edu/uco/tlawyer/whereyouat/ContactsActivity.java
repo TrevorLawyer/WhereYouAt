1c47d7ad3f2a5b4d11e00405bfb0849fe2f94f74
atoscani <atoscani1@uco.edu>
2017-10-29 12:46:04 -0500

    Fixing dependency issue

--- app/src/main/java/edu/uco/tlawyer/whereyouat/ContactsActivity.java
+++ app/src/main/java/edu/uco/tlawyer/whereyouat/ContactsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.Arrays;
+
 public class ContactsActivity extends Activity {
     String fname[]={"Petyr","Robert","Ramsey","Sandor","Khal","Tormund","Theon",
             "Jaime","Oberyn","Jorah","Daario","Podrick","Tyene","Davos",
@@ -14,6 +16,7 @@ public class ContactsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        Arrays.sort(fname);
         ListView list = (ListView) findViewById(R.id.contactList);
         ArrayAdapter<String> adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, fname);
