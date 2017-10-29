8719667f445e0b9ec0c7d3880985220646d21963
TrevorLawyer <trevorlawyer@gmail.com>
2017-10-29 13:06:31 -0500

    Test

--- app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
+++ app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
@@ -1,14 +1,14 @@
 package edu.uco.tlawyer.whereyouat;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     // Declare Varibles
     EditText usernameInput, passwordInput;
