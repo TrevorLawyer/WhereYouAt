bced94b64c9ba9bb7c3242538ea8d8c85d9d964e
TrevorLawyer <trevorlawyer@gmail.com>
2017-10-29 14:05:08 -0500

    Test

--- app/src/main/java/edu/uco/tlawyer/whereyouat/RegisterUserActivity.java
+++ app/src/main/java/edu/uco/tlawyer/whereyouat/RegisterUserActivity.java
@@ -1,13 +1,13 @@
 package edu.uco.tlawyer.whereyouat;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class RegisterUserActivity extends AppCompatActivity {
+public class RegisterUserActivity extends Activity {
 
     EditText regPassword, regEmail, regUsername, confirmPass;
     Button regRegister;
