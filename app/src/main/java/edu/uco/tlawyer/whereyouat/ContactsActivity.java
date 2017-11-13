7e6500cac408dcacb38bd51bca17c2fb324f3b9d
dbabb81 <david.babb81@gmail.com>
2017-11-12 19:39:11 -0600

    adding database tasks to ContactsActivity

--- app/src/main/java/edu/uco/tlawyer/whereyouat/ContactsActivity.java
+++ app/src/main/java/edu/uco/tlawyer/whereyouat/ContactsActivity.java
@@ -8,12 +8,21 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class ContactsActivity extends Activity {
-    static ArrayList<String> fname = new ArrayList<>();
+    ArrayList<String> fname = new ArrayList<>();
     ArrayAdapter<String> adapter;
+    UserClass currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,16 +44,41 @@ public class ContactsActivity extends Activity {
             }
         });
 
+        currentUser = new UserClass();
+//        currentUser.contactList.add("James");
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        ref.child("users").child(user.getUid()).child("contactList")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String databaseContact = dataSnapshot.getValue(String.class);
+                        currentUser.contactList.add(databaseContact);
+                        //currentUser.contactList.add(dataSnapshot.getValue(String.class));
+                    }
 
-        fname.add("Addison Toscani");
-        fname.add("David Babb");
-        fname.add("Seth Hoewll");
-        fname.add("Trevor Lawyer");
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+        fname.addAll(currentUser.contactList);
+//        fname.add("Addison Toscani");
+//        fname.add("David Babb");
+//        fname.add("Seth Howell");
+//        fname.add("Trevor Lawyer");
        Collections.sort(fname);
         ListView list = (ListView) findViewById(R.id.contactList);
         adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, fname);
         list.setAdapter(adapter);
+
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//        });
 
     }
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
