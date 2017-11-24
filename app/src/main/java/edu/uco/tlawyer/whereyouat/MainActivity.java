cda0ed80246f4d20a6b976890b1804f765f3465c
showell2121 <showell2121@gmail.com>
2017-11-24 08:05:06 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/libraries/animated_vector_drawable_25_2_0.xml
    #	.idea/libraries/appcompat_v7_25_2_0.xml
    #	.idea/libraries/constraint_layout_1_0_2.xml
    #	.idea/libraries/espresso_core_2_2_2.xml
    #	.idea/libraries/espresso_idling_resource_2_2_2.xml
    #	.idea/libraries/exposed_instrumentation_api_publish_0_5.xml
    #	.idea/libraries/firebase_analytics_11_0_4.xml
    #	.idea/libraries/firebase_analytics_impl_11_0_4.xml
    #	.idea/libraries/firebase_appindexing_11_0_4.xml
    #	.idea/libraries/firebase_auth_11_0_4.xml
    #	.idea/libraries/firebase_common_11_0_4.xml
    #	.idea/libraries/firebase_config_11_0_4.xml
    #	.idea/libraries/firebase_core_11_0_4.xml
    #	.idea/libraries/firebase_crash_11_0_4.xml
    #	.idea/libraries/firebase_database_11_0_4.xml
    #	.idea/libraries/firebase_database_connection_11_0_4.xml
    #	.idea/libraries/firebase_dynamic_links_11_0_4.xml
    #	.idea/libraries/firebase_iid_11_0_4.xml
    #	.idea/libraries/firebase_messaging_11_0_4.xml
    #	.idea/libraries/firebase_storage_11_0_4.xml
    #	.idea/libraries/firebase_storage_common_11_0_4.xml
    #	.idea/libraries/mediarouter_v7_25_2_0.xml
    #	.idea/libraries/palette_v7_25_2_0.xml
    #	.idea/libraries/play_services_11_0_4.xml
    #	.idea/libraries/play_services_ads_11_0_4.xml
    #	.idea/libraries/play_services_ads_lite_11_0_4.xml
    #	.idea/libraries/play_services_analytics_11_0_4.xml
    #	.idea/libraries/play_services_analytics_impl_11_0_4.xml
    #	.idea/libraries/play_services_appinvite_11_0_4.xml
    #	.idea/libraries/play_services_auth_11_0_4.xml
    #	.idea/libraries/play_services_auth_api_phone_11_0_4.xml
    #	.idea/libraries/play_services_auth_base_11_0_4.xml
    #	.idea/libraries/play_services_awareness_11_0_4.xml
    #	.idea/libraries/play_services_base_11_0_4.xml
    #	.idea/libraries/play_services_basement_11_0_4.xml
    #	.idea/libraries/play_services_cast_11_0_4.xml
    #	.idea/libraries/play_services_cast_framework_11_0_4.xml
    #	.idea/libraries/play_services_clearcut_11_0_4.xml
    #	.idea/libraries/play_services_drive_11_0_4.xml
    #	.idea/libraries/play_services_fitness_11_0_4.xml
    #	.idea/libraries/play_services_games_11_0_4.xml
    #	.idea/libraries/play_services_gass_11_0_4.xml
    #	.idea/libraries/play_services_gcm_11_0_4.xml
    #	.idea/libraries/play_services_identity_11_0_4.xml
    #	.idea/libraries/play_services_iid_11_0_4.xml
    #	.idea/libraries/play_services_instantapps_11_0_4.xml
    #	.idea/libraries/play_services_location_11_0_4.xml
    #	.idea/libraries/play_services_maps_11_0_4.xml
    #	.idea/libraries/play_services_nearby_11_0_4.xml
    #	.idea/libraries/play_services_panorama_11_0_4.xml
    #	.idea/libraries/play_services_places_11_0_4.xml
    #	.idea/libraries/play_services_plus_11_0_4.xml
    #	.idea/libraries/play_services_safetynet_11_0_4.xml
    #	.idea/libraries/play_services_tagmanager_11_0_4.xml
    #	.idea/libraries/play_services_tagmanager_api_11_0_4.xml
    #	.idea/libraries/play_services_tagmanager_v4_impl_11_0_4.xml
    #	.idea/libraries/play_services_tasks_11_0_4.xml
    #	.idea/libraries/play_services_vision_11_0_4.xml
    #	.idea/libraries/play_services_vision_common_11_0_4.xml
    #	.idea/libraries/play_services_wallet_11_0_4.xml
    #	.idea/libraries/play_services_wearable_11_0_4.xml
    #	.idea/libraries/rules_0_5.xml
    #	.idea/libraries/runner_0_5.xml
    #	.idea/libraries/support_compat_25_2_0.xml
    #	.idea/libraries/support_core_ui_25_2_0.xml
    #	.idea/libraries/support_core_utils_25_2_0.xml
    #	.idea/libraries/support_fragment_25_2_0.xml
    #	.idea/libraries/support_media_compat_25_2_0.xml
    #	.idea/libraries/support_v4_25_2_0.xml
    #	.idea/libraries/support_vector_drawable_25_2_0.xml
    #	app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
+++ app/src/main/java/edu/uco/tlawyer/whereyouat/MainActivity.java
@@ -2,7 +2,6 @@ package edu.uco.tlawyer.whereyouat;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
@@ -15,7 +14,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
+
+//import android.support.annotation.NonNull;
 
 public class MainActivity extends Activity {
 
@@ -51,61 +51,18 @@ public class MainActivity extends Activity {
             public void onClick(View view) {
 
                 //sets varables that were entered in
-                inputUsername = usernameInput.getText().toString();
+                inputUsername = usernameInput.getText().toString().trim();
                 inputPassword = passwordInput.getText().toString();
 
-                // System.out.println(" input :" +inputUsername + ":");
-
-                //checks if password and username are not empty
-                if (!inputPassword.matches("") && !inputUsername.matches("")) {
-
-                    if (inputPassword.length() < 6) {
-                        Toast.makeText(MainActivity.this, "Invalid Password Length", Toast.LENGTH_SHORT).show();
-                    } else {
-
-//                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-//                        String name = user.getDisplayName();
-
-                        singinAuth.signInWithEmailAndPassword(inputUsername, inputPassword)
-                                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if (task.isSuccessful()) {
-
-                                            //check password and username before proceeding to login page
-                                            Intent intent = new Intent(MainActivity.this, LoginSuccessActivity.class);
-                                            intent.putExtra("USERNAME", inputUsername);
-                                            intent.putExtra("PASSWORD", inputPassword);
-                                            startTrackerService();
-                                            startActivityForResult(intent, 1);
-
-
-                                        } else {
-                                            // failed ot log in
-                                            Toast.makeText(MainActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
-                                        }
-
-                                    }
-                                });
-
-
-                    }
-
-                } else {
-                    Toast.makeText(MainActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
-                }
+                //Toast.makeText(MainActivity.this, "Email: " + inputUsername, Toast.LENGTH_SHORT).show();
+                checkEmail(inputUsername);
             }
         });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // String key = dbRef.push().getKey();
-
-                // dbRef.child(key).setValue("hwllow, jkldsj");
-
-
-                // dbRef.push();
+               //forward to create new account
                 Intent intent = new Intent(MainActivity.this, RegisterUserActivity.class);
                 startActivityForResult(intent, 2);
 
@@ -113,6 +70,72 @@ public class MainActivity extends Activity {
         });
 
     }
+
+    public void checkEmail(final String emailX) {
+
+        String password2 = "0000000000";
+
+        //checks if email is empty
+        if(!emailX.equals("")) {
+
+            //authenticats user email by seeing if it allows to create an account
+            singinAuth.createUserWithEmailAndPassword(emailX, password2)
+                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            //account create user does not exist.
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                //delete user just created
+                                user.delete();
+                                //prompt user to create account
+                                Toast.makeText(MainActivity.this, "No Email Exist ", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "Create New Account ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // if failed, email already exist and move on to verify passoword
+                                //Toast.makeText(MainActivity.this, "Loggin In", Toast.LENGTH_SHORT).show();
+                                signUserIn(emailX);
+                            }
+
+                        }
+                    });
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Fill Out Email Address", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void signUserIn(String email){
+
+        inputPassword = passwordInput.getText().toString();
+
+        if(!inputPassword.equals("")) {
+            //checks if user password matches user email
+            singinAuth.signInWithEmailAndPassword(email, inputPassword)
+                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            //user password and email match
+                            if (task.isSuccessful()) {
+
+                                //check password and username before proceeding to login page
+                                Intent intent = new Intent(MainActivity.this, LoginSuccessActivity.class);
+                                intent.putExtra("USERNAME", inputUsername);
+                                intent.putExtra("PASSWORD", inputPassword);
+                                startTrackerService();
+                                startActivityForResult(intent, 1);
+                            } else {
+                                // password does not match email
+                                Toast.makeText(MainActivity.this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }else{
+            Toast.makeText(MainActivity.this, "Fill Out Password", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
     private void startTrackerService() {
         Intent i = new Intent(this, TrackerService.class);
@@ -123,57 +146,56 @@ public class MainActivity extends Activity {
     }
 
     public void passwordReset(View v) {
-        //loop through all auth users
-        int counter = 0;
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        String password2 = "0000000000";
+
+        //gets email address
+        String inputUsername = usernameInput.getText().toString().trim();
+
+        //checks if email is empty and is a gmail account
+        if(!inputUsername.equals("")) {
+
+            //authenticats user email by seeing if it allows to create an account
+            singinAuth.createUserWithEmailAndPassword(inputUsername, password2)
+                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            //account create user does not exist.
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                //delete user just created
+                                user.delete();
+                                //prompt user to create account
+                                Toast.makeText(MainActivity.this, "No Email Exist ", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "Create New Account ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // if failed, email already exist and move on to verify passoword
+                                //Toast.makeText(MainActivity.this, "Loggin In", Toast.LENGTH_SHORT).show();
+                                sentReset();
+                            }
+                        }
+                    });
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Fill Out Email Address", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void sentReset(){
         //reset password part
         FirebaseAuth auth = FirebaseAuth.getInstance();
         String emailAddress = usernameInput.getText().toString();
-        // checks if user null
-        if (user != null && !emailAddress.equals("")) {
-            //Toast.makeText(MainActivity.this, "in IF ", Toast.LENGTH_SHORT).show();
-            // going through all athenticated users
-            for (UserInfo profile : user.getProviderData()) {
-                //Toast.makeText(MainActivity.this, "in FOR ", Toast.LENGTH_SHORT).show();
-                // Id of the provider (ex: google.com)
-                String providerId = profile.getProviderId();
 
-                // UID specific to the provider
-                String uid = profile.getUid();
-
-                // Name, email address, and profile photo Url
-                String name = profile.getDisplayName();
-                String email = profile.getEmail();
-                Uri photoUrl = profile.getPhotoUrl();
-
-                //if statement to check if emailed enter matches email the for loop is looking at
-                if (profile.getEmail().equals(emailAddress)) {
-                    //incrememnts int to show password reset is happening
-                    counter++;
-                    //sends email to user to reset password
-                    auth.sendPasswordResetEmail(emailAddress)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(MainActivity.this, "Reset Successful ", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-
-
-                }
-            }
-            //checks if counter was incremented by the email being in the database
-            if (counter != 0) {
-                Toast.makeText(MainActivity.this, "Password Reset has been Emailed ", Toast.LENGTH_SHORT).show();
-            } else{
-                Toast.makeText(MainActivity.this, "No Email Exist ", Toast.LENGTH_SHORT).show();
-                Toast.makeText(MainActivity.this, "Create New Account ", Toast.LENGTH_SHORT).show();}
-        }else
-            Toast.makeText(MainActivity.this, "Fill Out Email Address ", Toast.LENGTH_SHORT).show();
-
+        //sends email to user to reset password
+        auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Password Reset has been Emailed ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 }
